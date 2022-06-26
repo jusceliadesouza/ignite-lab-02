683c3d17cf8f242f98ef0ea4b339f54474ad3aee
Juscélia de Souza <jusceliadesousa@gmail.com>
2022-06-25 22:12:17 -0300

    Aula 05 • Deploy, CodeGen e próximos passos

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,50 +1,22 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
-import { gql, useQuery } from "@apollo/client";
 
 import { CaretRight, DiscordLogo, FileArrowDown, Image, Lightning } from "phosphor-react";
 
 import "@vime/core/themes/default.css"
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug($slug: String) {
-  lesson(where: {slug: $slug}) {
-    title
-    videoId
-    description
-    teacher {
-      bio
-      avatarURL
-      name
-    }
-  }
-}
-`
-
-interface GetLessonsBySlugResponse {
-  lesson: {
-    title: string
-    videoId: string
-    description: string
-    teacher: {
-      bio: string
-      avatarURL: string
-      name: string
-    }
-  }
-}
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 interface VideoProps {
   lessonSlug: string
 }
 
 export function Video(props: VideoProps) {
-  const { data } = useQuery<GetLessonsBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
       slug: props.lessonSlug
     }
   })
 
-  if (!data) {
+  if (!data || !data.lesson) {
     return (
       <div className="flex-1">
         <p>Carregando...</p>
@@ -74,16 +46,18 @@ export function Video(props: VideoProps) {
               {data.lesson.description}
             </p>
 
-            <div className="flex items-center gap-4 mt-6">
-              <img
-                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src="https://github.com/diego3g.png"
-                alt=""
-              />
+            {data.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6">
+                <img
+                  className="h-16 w-16 rounded-full border-2 border-blue-500"
+                  src="https://github.com/diego3g.png"
+                  alt=""
+                />
 
-              <div className="font-bold text-2xl block leading-relaxed">{data.lesson.teacher.name}</div>
-              <span className="text-gray-200 text-sm">{data.lesson.teacher.bio}</span>
-            </div>
+                <div className="font-bold text-2xl block leading-relaxed">{data.lesson.teacher.name}</div>
+                <span className="text-gray-200 text-sm">{data.lesson.teacher.bio}</span>
+              </div>
+            )}
           </div>
 
           <div className="flex flex-col gap-4">
@@ -91,7 +65,7 @@ export function Video(props: VideoProps) {
               <DiscordLogo size={24} />
               Comunidade do Discord
             </a>
-            
+
             <div className="flex flex-col gap-4">
               <a href="" className="p-4 text-sm border border-blue-200 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
                 <Lightning size={24} />
@@ -99,7 +73,6 @@ export function Video(props: VideoProps) {
               </a>
             </div>
           </div>
-
 
         </div>
         <div className="gap-8 mt-20 grid grid-cols-2">
